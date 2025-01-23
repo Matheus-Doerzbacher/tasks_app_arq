72fb9a9e4553bc31bf7091c9be61d7f10169934f
Matheus Doerzbacher <matheus.doer@gmail.com>
2025-01-23 20:30:16 -0300

    Implement authentication with JWT, password hashing, and token storage

--- lib/data/repositories/auth/auth_repository_remote.dart
+++ lib/data/repositories/auth/auth_repository_remote.dart
@@ -1,5 +1,7 @@
+import 'package:logging/logging.dart';
 import 'package:tasks_app_arq/data/repositories/auth/auth_repository.dart';
 import 'package:tasks_app_arq/data/services/firebase/user/user_firebase_client.dart';
+import 'package:tasks_app_arq/data/services/shared_preferences_service.dart';
 import 'package:tasks_app_arq/utils/result.dart';
 
 class AuthRepositoryRemote extends AuthRepository {
@@ -12,20 +14,69 @@ class AuthRepositoryRemote extends AuthRepository {
   })  : _userFirebaseClient = userFirebaseClient,
         _sharedPreferencesService = sharedPreferencesService;
 
-  @override
-  // TODO: implement isAuthenticated
-  Future<bool> get isAuthenticated => throw UnimplementedError();
+  bool? _isAuthenticated;
+  final _log = Logger('AuthRepositoryRemote');
 
-  @override
-  Future<Result<void>> login(
-      {required String email, required String password}) {
-    // TODO: implement login
-    throw UnimplementedError();
+  /// Fetch token from shared preferences
+  Future<void> _fetch() async {
+    final result = await _sharedPreferencesService.fetchToken();
+    switch (result) {
+      case Ok<String?>():
+        _isAuthenticated = result.value != null;
+      case Error<String?>():
+        _log.severe(
+          'Failed to fech Token from SharedPreferences',
+          result.error,
+        );
+    }
   }
 
   @override
-  Future<Result<void>> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<bool> get isAuthenticated async {
+    // Status is cached
+    if (_isAuthenticated != null) {
+      return _isAuthenticated!;
+    }
+    // No status cached, fetch from storage
+    await _fetch();
+    return _isAuthenticated ?? false;
+  }
+
+  @override
+  Future<Result<void>> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final result = await _userFirebaseClient.loginUser(email, password);
+
+      switch (result) {
+        case Ok<String>():
+          _log.info('User logged int');
+          _isAuthenticated = true;
+          return await _sharedPreferencesService.saveToken(result.value);
+        case Error<String>():
+          _log.warning('Error logging in: ${result.error}');
+          return Result.error(result.error);
+      }
+    } finally {
+      notifyListeners();
+    }
+  }
+
+  @override
+  Future<Result<void>> logout() async {
+    _log.info('User logged out');
+    try {
+      // Passar um valor nulo faz com que o token seja eliminado
+      final result = await _sharedPreferencesService.saveToken(null);
+      if (result is Error<void>) {
+        _log.severe('Failed to clear stored auth token');
+      }
+      _isAuthenticated = false;
+      return result;
+    } finally {
+      notifyListeners();
+    }
   }
 }
--- lib/data/services/firebase/user/user_firebase_client.dart
+++ lib/data/services/firebase/user/user_firebase_client.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crypto/crypto.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:tasks_app_arq/domain/models/user/user.dart';
+import 'package:tasks_app_arq/jwt_secret.dart';
 import 'package:tasks_app_arq/utils/result.dart';
 
 final userCollection = 'users';
@@ -7,10 +12,30 @@ final userCollection = 'users';
 class UserFirebaseClient {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  Future<Result<String>> loginUser(String email, String password) async {
+    try {
+      final result = await _getUserByEmail(email);
+      switch (result) {
+        case Ok<User>():
+          final passwordHash = await _hashPassword(password);
+          if (result.value.password != passwordHash) {
+            return Result.error(Exception('Invalid password'));
+          }
+          final jwt = JWT(result.value);
+          return Result.ok(jwt.sign(SecretKey(jwtTokenKey)));
+        case Error<User>():
+          return Result.error(result.error);
+      }
+    } catch (e) {
+      return Result.error(Exception(e));
+    }
+  }
+
   Future<Result<User>> addUser(User user) async {
     try {
       final userId = _firestore.collection(userCollection).doc().id;
-      final userWithId = user.copyWith(id: userId);
+      final passwordHash = await _hashPassword(user.password);
+      final userWithId = user.copyWith(id: userId, password: passwordHash);
       await _firestore
           .collection(userCollection)
           .doc(userId)
@@ -40,5 +65,27 @@ class UserFirebaseClient {
     } catch (e) {
       return Result.error(Exception(e));
     }
+  }
+
+  Future<Result<User>> _getUserByEmail(String email) async {
+    try {
+      final user = await _firestore
+          .collection(userCollection)
+          .where('email', isEqualTo: email)
+          .get();
+
+      if (user.docs.isEmpty) {
+        return Result.error(Exception('User not found'));
+      }
+
+      final userData = user.docs.first.data();
+      return Result.ok(User.fromJson(userData));
+    } catch (e) {
+      return Result.error(Exception(e));
+    }
+  }
+
+  Future<String> _hashPassword(String password) async {
+    return sha256.convert(utf8.encode(password)).toString();
   }
 }
