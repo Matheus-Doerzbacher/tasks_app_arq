f4bbc4cdac3b9c1c5e04e55a23ef3000fe8db55f
Matheus Doerzbacher <matheus.doer@gmail.com>
2025-01-24 23:04:16 -0300

    Improve login and signup UI with scrollable forms and responsive layout

--- lib/ui/user/login/widgets/login_screen.dart
+++ lib/ui/user/login/widgets/login_screen.dart
@@ -61,232 +61,243 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Widget _buildLoginPage() {
-    return Padding(
-      padding: const EdgeInsets.all(24),
-      child: Form(
-        key: _formLogin,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(8),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Form(
+          key: _formLogin,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 50),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: Image.asset('assets/images/task.png', width: 400),
               ),
-              padding: const EdgeInsets.all(8),
-              child: Image.asset('assets/images/task.png', width: 400),
-            ),
-            const SizedBox(height: 100),
-            TextFormField(
-              controller: _email,
-              decoration: InputDecoration(labelText: 'Email'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Email é obrigatório';
-                }
+              const SizedBox(height: 50),
+              TextFormField(
+                controller: _email,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Email é obrigatório';
+                  }
 
-                if (!RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
-                    .hasMatch(value)) {
-                  return 'Email inválido';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _password,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Senha é obrigatório';
-                }
-
-                if (value.length < 6) {
-                  return 'Senha deve ter pelo menos 6 caracteres';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            ListenableBuilder(
-              listenable: widget.viewModel.login,
-              builder: (context, _) {
-                return FilledButton(
-                  onPressed: widget.viewModel.login.running
-                      ? null
-                      : () {
-                          if (_formLogin.currentState!.validate()) {
-                            widget.viewModel.login.execute(
-                              (_email.value.text, _password.value.text),
-                            );
-                          }
-                        },
-                  child: widget.viewModel.login.running
-                      ? SizedBox(
-                          width: 24,
-                          height: 24,
-                          child: CircularProgressIndicator(strokeWidth: 2),
-                        )
-                      : const Text('Login'),
-                );
-              },
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: () {
-                _pageController.animateToPage(
-                  1, // Índice da página de criação de conta
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeInOut,
-                );
-              },
-              child: Text(
-                'Criar uma conta',
-                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+                  if (!RegExp(
+                          r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+                      .hasMatch(value)) {
+                    return 'Email inválido';
+                  }
+                  return null;
+                },
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _password,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Senha é obrigatório';
+                  }
+
+                  if (value.length < 6) {
+                    return 'Senha deve ter pelo menos 6 caracteres';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ListenableBuilder(
+                listenable: widget.viewModel.login,
+                builder: (context, _) {
+                  return FilledButton(
+                    onPressed: widget.viewModel.login.running
+                        ? null
+                        : () {
+                            if (_formLogin.currentState!.validate()) {
+                              widget.viewModel.login.execute(
+                                (_email.value.text, _password.value.text),
+                              );
+                            }
+                          },
+                    child: widget.viewModel.login.running
+                        ? SizedBox(
+                            width: 24,
+                            height: 24,
+                            child: CircularProgressIndicator(strokeWidth: 2),
+                          )
+                        : const Text('Login'),
+                  );
+                },
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () {
+                  _pageController.animateToPage(
+                    1, // Índice da página de criação de conta
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                child: Text(
+                  'Criar uma conta',
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget _buildSignUpPage() {
-    return Padding(
-      padding: const EdgeInsets.all(24),
-      child: Form(
-        key: _formSignUp,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(8),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Form(
+          key: _formSignUp,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: Image.asset('assets/images/user.png', width: 400),
               ),
-              padding: const EdgeInsets.all(8),
-              child: Image.asset('assets/images/user.png', width: 400),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _email,
-              decoration: InputDecoration(labelText: 'Email'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Email é obrigatório';
-                }
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _email,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Email é obrigatório';
+                  }
 
-                if (!RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
-                    .hasMatch(value)) {
-                  return 'Email inválido';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _name,
-              decoration: InputDecoration(labelText: 'Name'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Nome é obrigatório';
-                }
-
-                if (value.length < 3) {
-                  return 'Nome deve ter pelo menos 3 caracteres';
-                }
-
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _password,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Senha é obrigatório';
-                }
-
-                if (value.length < 6) {
-                  return 'Senha deve ter pelo menos 6 caracteres';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _confirmPassword,
-              decoration: InputDecoration(labelText: 'Confirm Password'),
-              obscureText: true,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Senha é obrigatório';
-                }
-
-                if (value != _password.value.text) {
-                  return 'As senhas não correspondem';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            ListenableBuilder(
-              listenable: widget.viewModel.createUser,
-              builder: (context, _) {
-                return FilledButton(
-                  onPressed: widget.viewModel.createUser.running
-                      ? null
-                      : () {
-                          if (_formSignUp.currentState!.validate()) {
-                            final user = User(
-                              email: _email.value.text,
-                              name: _name.value.text,
-                              password: _password.value.text,
-                            );
-                            widget.viewModel.createUser.execute(user);
-                            _name.clear();
-                            _password.clear();
-                            _confirmPassword.clear();
-                            _pageController.animateToPage(
-                              0, // Índice da página de criação de conta
-                              duration: const Duration(milliseconds: 300),
-                              curve: Curves.easeInOut,
-                            );
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text('Usuário criado com sucesso'),
-                              ),
-                            );
-                          }
-                        },
-                  child: widget.viewModel.createUser.running
-                      ? SizedBox(
-                          width: 24,
-                          height: 24,
-                          child: CircularProgressIndicator(strokeWidth: 2),
-                        )
-                      : const Text('Criar conta'),
-                );
-              },
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: () {
-                _pageController.animateToPage(
-                  0, // Índice da página de criação de conta
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeInOut,
-                );
-              },
-              child: Text(
-                'Já tem uma conta? Faça login',
-                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+                  if (!RegExp(
+                          r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+                      .hasMatch(value)) {
+                    return 'Email inválido';
+                  }
+                  return null;
+                },
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _name,
+                decoration: InputDecoration(labelText: 'Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nome é obrigatório';
+                  }
+
+                  if (value.length < 3) {
+                    return 'Nome deve ter pelo menos 3 caracteres';
+                  }
+
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _password,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Senha é obrigatório';
+                  }
+
+                  if (value.length < 6) {
+                    return 'Senha deve ter pelo menos 6 caracteres';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _confirmPassword,
+                decoration: InputDecoration(labelText: 'Confirm Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Senha é obrigatório';
+                  }
+
+                  if (value != _password.value.text) {
+                    return 'As senhas não correspondem';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ListenableBuilder(
+                listenable: widget.viewModel.createUser,
+                builder: (context, _) {
+                  return FilledButton(
+                    onPressed: widget.viewModel.createUser.running
+                        ? null
+                        : () {
+                            if (_formSignUp.currentState!.validate()) {
+                              final user = User(
+                                email: _email.value.text,
+                                name: _name.value.text,
+                                password: _password.value.text,
+                              );
+                              widget.viewModel.createUser.execute(user);
+                              _name.clear();
+                              _password.clear();
+                              _confirmPassword.clear();
+                              _pageController.animateToPage(
+                                0, // Índice da página de criação de conta
+                                duration: const Duration(milliseconds: 300),
+                                curve: Curves.easeInOut,
+                              );
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text('Usuário criado com sucesso'),
+                                ),
+                              );
+                            }
+                          },
+                    child: widget.viewModel.createUser.running
+                        ? SizedBox(
+                            width: 24,
+                            height: 24,
+                            child: CircularProgressIndicator(strokeWidth: 2),
+                          )
+                        : const Text('Criar conta'),
+                  );
+                },
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () {
+                  _pageController.animateToPage(
+                    0, // Índice da página de criação de conta
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                child: Text(
+                  'Já tem uma conta? Faça login',
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
